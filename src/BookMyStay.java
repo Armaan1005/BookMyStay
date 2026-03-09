c53775d28d258f124f631158ea8b0d69df7b9773
Armaan1005 <patel.armaan1005@gmail.com>
2026-03-09 09:12:43 +0530

    Base application setup

---
+++ src/BookMyStay.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Booking Application");
+
+    }
+
+}
